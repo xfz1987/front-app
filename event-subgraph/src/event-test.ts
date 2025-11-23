6e8b5ea8a251c7addfbcf21a8fea18a38993bbbd
xfz1987 <281345774@qq.com>
2025-11-23 09:34:12 +0800

    check issure

--- event-subgraph/src/event-test.ts
+++ event-subgraph/src/event-test.ts
@@ -1,17 +1,17 @@
-import { Transfer as TransferEvent } from "../generated/EventTest/EventTest"
-import { Transfer } from "../generated/schema"
+import { Transfer as TransferEvent } from '../generated/EventTest/EventTest';
+import { Transfer } from '../generated/schema';
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._from = event.params._from
-  entity._to = event.params._to
-  entity._value = event.params._value
+	const entity = new Transfer(
+		event.transaction.hash.concatI32(event.logIndex.toI32())
+	);
+	entity._from = event.params._from;
+	entity._to = event.params._to;
+	entity._value = event.params._value;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+	entity.blockNumber = event.block.number;
+	entity.blockTimestamp = event.block.timestamp;
+	entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+	entity.save();
 }
